65fcd68a70a86b2d2b2da7d997d0e080359dba14
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2018-06-11 14:06:02 +0100

    open links new tab

--- locales/locales.js
+++ locales/locales.js
@@ -35,10 +35,22 @@ var locales = {
   },
   "global": {
     "social icon": {
-      "twitter": "twitter",
-      "facebook": "facebook",
-      "instagram": "instagram",
-      "youtube": "youtube",
+      "twitter": {
+        name: "twitter",
+        url: "https://twitter.com/FST_Lisboa",
+      },
+      "facebook": {
+        name: "facebook",
+        url: "https://www.facebook.com/fstlisboa/",
+      },
+      "instagram": {
+        name: "instagram",
+        url: "https://www.instagram.com/fst_lisboa/",
+      },
+      "youtube": {
+        name: "youtube",
+        url: "https://www.youtube.com/user/ProjectoFST",
+      },
     }
   }
 }
